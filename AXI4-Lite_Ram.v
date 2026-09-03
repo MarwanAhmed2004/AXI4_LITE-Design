0c9aecbbd36863f82b21ef8d9863f8432eff286c
Marwan <marwanmarwanahmed8@gmail.com>
2026-09-03 20:38:57 +0300

    Refactor AXI4-Lite_Ram module for parameterization

--- AXI4-Lite_Ram.v
+++ AXI4-Lite_Ram.v
@@ -1,15 +1,14 @@
-module AXI4_Lite_Ram (
+module AXI4_Lite_Ram #(parameter DAT_PAR = 32 )(
     ACLK_RAM,ARESETN_RAM,ARADDR_RAM,
     AWADDR_RAM,WDATA_RAM,RDATA_RAM,ARREADY_RAM,WREADY_RAM
 );
-    parameter DAT_PAR = 32 ;
     input [DAT_PAR-1:0] WDATA_RAM ;
     input [7:0] ARADDR_RAM , AWADDR_RAM ;
     input ACLK_RAM , ARESETN_RAM , ARREADY_RAM , WREADY_RAM ;
     output reg [DAT_PAR-1:0] RDATA_RAM ;
     reg [DAT_PAR-1:0] mem [255:0] ;
 
-    always @(posedge ACLK_RAM or negedge ARESETN_RAM) begin
+    always @(posedge ACLK_RAM ) begin
         if (~ARESETN_RAM) begin
             RDATA_RAM <= 32'h00000000 ;
         end 
